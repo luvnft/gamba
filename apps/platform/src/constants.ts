9873ca37ce2070cfd3ac38efdcf67e5075a56551
The Wizard of Hahz <info@blkluv.org>
2024-01-29 13:41:22 -0500

    updated

--- apps/platform/src/constants.ts
+++ apps/platform/src/constants.ts
@@ -1,6 +1,6 @@
 import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 
-export const PLATFORM_CREATOR_ADDRESS = 'V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9'
+export const PLATFORM_CREATOR_ADDRESS = '2qjX4as5UsfHkjpwuoVgUCtySTHtE5SBZEbNs1MrP4rR'
 
 /** Appears in ShareModal */
 export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
