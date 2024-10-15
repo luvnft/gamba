e9361197c188d2aaf5bff44129c2953061beb412
The Wizard of Hahz <info@luvnft.com>
2024-10-15 17:14:26 -0400

    updated

--- apps/platform/src/sections/Dashboard/WelcomeBanner.tsx
+++ apps/platform/src/sections/Dashboard/WelcomeBanner.tsx
@@ -5,18 +5,106 @@ import styled from 'styled-components'
 import { useUserStore } from '../../hooks/useUserStore'
 
 const Buttons = styled.div`
-  // styles here
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+
+  @media (min-width: 800px) {
+    height: 100%;
+  }
+
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    padding-top: 0!important;
+  }
+
+  & > button {
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    padding: 10px;
+    background: #ffffffdf;
+    transition: background-color .2s ease;
+    color: black;
+    cursor: pointer;
+    &:hover {
+      background: white;
+    }
+  }
 `
 
 const Welcome = styled.div`
-  // styles here
+  @keyframes welcome-fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes backgroundGradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  background: linear-gradient(-45deg, #ffb07c, #ff3e88, #2969ff, #ef3cff, #ff3c87);
+  background-size: 300% 300%;
+  animation: welcome-fade-in .5s ease, backgroundGradient 30s ease infinite;
+  border-radius: 10px;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+  filter: drop-shadow(0 4px 3px rgba(0,0,0,.07)) drop-shadow(0 2px 2px rgba(0,0,0,.06));
+
+  & img {
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    width: 100px;
+    height: 100px;
+    top: 0;
+    right: 0;
+    &:nth-child(1) {animation-delay: 0s;}
+    &:nth-child(2) {animation-delay: 1s;}
+  }
+
+  & > div {
+    padding: 0px;
+    filter: drop-shadow(0 4px 3px rgba(0,0,0,.07)) drop-shadow(0 2px 2px rgba(0,0,0,.06));
+  }
+
+  @media (min-width: 800px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    padding: 0;
+    & > div {
+      padding: 40px;
+    }
+  }
 `
 
 export function WelcomeBanner() {
   const wallet = useWallet()
   const walletModal = useWalletModal()
   const store = useUserStore()
-
   const copyInvite = () => {
     store.set({ userModal: true })
     if (!wallet.connected) {
@@ -27,15 +115,24 @@ export function WelcomeBanner() {
   return (
     <Welcome>
       <div>
-        <h1>✨ Welcome to MetaVegas.live</h1>
+        <h1>Welcome to Gamba v2 👋</h1>
         <p>
-        A fair, simple, and decentralized social charity casino on Solana that gives back to players who go live. Create a <a href='https://phantom.app/'><b>Phantom</b></a> wallet account. Then buy <a href='https://crypto.link.com?ref=lb&destination_currency=sol&destination_network=solana'><b>$SOL</b></a> or <a href='https://crypto.link.com?ref=lb&destination_currency=usdc&destination_network=solana'><b>$USDC</b></a> to start.
+          A fair, simple and decentralized casino on Solana.
         </p>
       </div>
       <Buttons>
-        <button onClick={() => window.open('https://soleye.luvnft.com', '_blank')}>🧿 Verify $SOL</button>
-        <button onClick={() => window.open('https://instagram.com/mvegaslive', '_blank')}>🪙 Instagram</button>
-        <button onClick={() => window.open('https://twitter.com/mvegaslive', '_blank')}>✨ Twitter</button>
+        <button onClick={copyInvite}>
+          💸 Copy Invite
+        </button>
+        <button onClick={() => window.open('https://soleye.luvnft.com', '_blank')}>
+          🧿 Verify $SOL
+        </button>
+        <button onClick={() => window.open('https://instagram.com/mvegaslive', '_blank')}>
+          💬 Instagram
+        </button>
+        <button onClick={() => window.open('https://twitter.com/mvegaslive', '_blank')}>
+          💬 Twitter
+        </button>
       </Buttons>
     </Welcome>
   )
